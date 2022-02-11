65408393ffd5f084c73556e9a2c872b655b92af2
Md Saber Hossain <mhmcb@missouri.edu>
2022-02-11 17:40:45 -0600

    [Task]: initial configuration

--- i2b2_config_data.js
+++ i2b2_config_data.js
@@ -6,9 +6,10 @@
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
 		{ domain: "i2b2demo",
-		  name: "HarvardDemo",
+		  name: "Missouri-BMI",
 		  urlCellPM: "http://services.i2b2.org/i2b2/services/PMService/",
 		  allowAnalysis: true,
+		  adminOnly: false,
 		  //installer: "/webclient/plugin_installer/",
 		  debug: false
 		}
