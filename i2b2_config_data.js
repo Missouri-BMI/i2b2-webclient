6b5155730b630e40c3a766a34e673fc5df53eb8d
Md Saber Hossain <mhmcb@missouri.edu>
2024-01-07 21:39:52 -0600

    local domain

--- i2b2_config_data.js
+++ i2b2_config_data.js
@@ -5,36 +5,16 @@
 	//-------------------------------------------------------------------------------------------
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
-          //      {
-          //              name: "i2b2demo_local",
-          //              domain: "i2b2demo",
-          //              urlCellPM: "http://127.0.0.1:9090/i2b2/services/PMService/",
-          //              allowAnalysis: true,
-          //              registrationMethod: "local",
-          //              loginType: "local",
-          //              showRegistration: true,
-          //              debug: true
-          //      }
-          //      ,
-          //      {
-          //              name: "i2b2demo_saml",
-          //              domain: "i2b2demo",
-          //              urlCellPM: "http://127.0.0.1/i2b2/services/PMService/",
-          //              allowAnalysis: true,
-          //              registrationMethod: "saml",
-          //              loginType: "federated",
-          //              showRegistration: true,
-          //              debug: true
-          //      }
-          //      ,				
-		{ 
-				name: "i2b2 Demo",
-				domain: "i2b2demo",
-				urlCellPM: "http://services.i2b2.org/i2b2/services/PMService/",
-				allowAnalysis: false,
-				debug: true,
-				isSHRINE: false
-		}
+                {
+                        domain: "i2b2demo",
+                        name: "HarvardDemo",
+                        allowAnalysis: true,
+                        urlCellPM: "http://i2b2-wildfly:8080/i2b2/services/PMService/",
+                        registrationMethod: "",
+                        loginType: "local",
+                        showRegistration: false,
+                        debug: true
+                    }
 	]
 	//-------------------------------------------------------------------------------------------
 }
